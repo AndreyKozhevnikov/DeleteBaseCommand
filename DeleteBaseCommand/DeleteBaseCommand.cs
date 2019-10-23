81728d501ffb699725ad905e6adf83cdffcd56a0
Andrey <freazeek@mail.ru>
2019-10-23 10:47:27 +0300

    fix can't find a dbname
    fix can't find a dbname

--- DeleteBaseCommand/DeleteBaseCommand.cs
+++ DeleteBaseCommand/DeleteBaseCommand.cs
@@ -13,6 +13,8 @@ using EnvDTE;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Linq;
 
 namespace DeleteBaseCommand {
     /// <summary>
@@ -96,15 +98,27 @@ namespace DeleteBaseCommand {
             List<string> configFiles = new List<string>();
             configFiles.AddRange(Directory.GetFiles(solutionFolderName, "app.config", SearchOption.AllDirectories));
             configFiles.AddRange(Directory.GetFiles(solutionFolderName, "web.config", SearchOption.AllDirectories));
-            var dbNamePattern = @"<add name=""ConnectionString"" connectionString=""Integrated Security=SSPI;Pooling=false;Data Source=\(localdb\)\\mssqllocaldb;Initial Catalog=(?<dbname>.*)""";
+           // var dbNamePattern = @"<add name=""ConnectionString"" connectionString=""Integrated Security=SSPI;Pooling=false;Data Source=\(localdb\)\\mssqllocaldb;Initial Catalog=(?<dbname>.*)""";
             foreach(var confFile in configFiles) {
                 using(var sw = new StreamReader(confFile)) {
-                    var configText = sw.ReadToEnd();
-                    var dbNameRegex = new Regex(dbNamePattern);
-                    Match dbNameMatch = dbNameRegex.Match(configText);
-                    if(dbNameMatch.Success) {
+                    var xDocument = XDocument.Load(confFile);
+                    var el = xDocument.Root;
+                    var el2 = xDocument.Root.Elements();
+                    var configNode = xDocument.Root.Elements().Where(x => x.Name.LocalName == "connectionStrings").First();
+                    var configs = configNode.Elements();
+                    var nameXName = XName.Get("name", configNode.Name.Namespace.NamespaceName);
+
+                    var realConfig = configs.Where(x => x.Attribute(nameXName).Value == "ConnectionString").FirstOrDefault();
+                   
+                    if(realConfig!=null) {
+                        var nameXConnectionString = XName.Get("connectionString", configNode.Name.Namespace.NamespaceName);
+                        var connectionString  = realConfig.Attribute(nameXConnectionString).Value;
+                        var dbNamePattern = @"Initial Catalog=(?<dbname>.*)";
+                        var dbNameRegex = new Regex(dbNamePattern);
+                        Match dbNameMatch = dbNameRegex.Match(connectionString);
                         var dbName = dbNameMatch.Groups["dbname"].Value;
                         DeleteDb(dbName);
+                        return;
                         //         VsShellUtilities.ShowMessageBox(this.ServiceProvider,
                         //string.Format("db {0} was deleted", dbName),
                         //       "Deleted",
